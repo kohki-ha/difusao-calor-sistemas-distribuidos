3c3bcf535df585d905aaeb779fabf3af5cc187de
Thales Hasegawa <kohkihasegawa151@gmail.com>
2025-11-11 21:32:46 -0300

    chore: initial commit

---
+++ src/main/java/vitorkaneko/difusaocalor/DifusaoCalor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vitorkaneko.difusaocalor;
+
+/**
+ *
+ * @author Kaneko
+ */
+public class DifusaoCalor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
